fc708e2f2cf5490f0bb9bf3e05d15310820d96a3
gremerritt <gremerritt@gmail.com>
2016-11-15 20:05:03 -0500

    Add ability to send tweets Use groups for collecting tweets

--- app.rb
+++ app.rb
@@ -2,11 +2,64 @@ require 'twitter'
 require 'yaml'
 require 'json'
 
-def main method, tag, num
-  opts = ['collect', 'target']
-  if !opts.include? method
+def main method, input_options
+  methods = ['collect', 'target']
+  groups = [:cybersecurity, :politics, :science, :sports]
+  options = {:hashtag => nil,
+             :count => 10,
+             :user => nil,
+             :send => false,
+             :group => :default,
+             :links => false}
+  if !methods.include? method
     puts "Invalid option: #{method}"
-    puts "Valid commands are:\n  #{opts.join("\n  ")}"
+    puts "Valid commands are:\n  #{methods.join("\n  ")}"
+    return
+  end
+
+  # get the inputs
+  input_options.each do |opt_str|
+    opt_split = opt_str.split '='
+    begin
+      opt = opt_split[0].split('--')[1].to_sym
+      val = opt_split[1]
+
+      if !options.include? opt
+        puts "Invalid option #{opt}"
+      else
+        case opt
+        when :hashtag
+          options[:hashtag] = val.strip
+        when :user
+          options[:user] = val.strip
+        when :count
+          if /^[[:digit:]]*$/.match(val)
+            options[opt] = val.to_i
+          else
+            puts "Option '#{opt}' must be a valid number"
+            return
+          end
+        when :send
+          options[:send] = (val == 'true') ? true : false
+        when :group
+          if groups.include? val.to_sym
+            options[:group] = val.to_sym
+          else
+            puts "Invalid group '#{val}'. Options are\n  #{groups.join("\n  ")}"
+            return
+          end
+        when :links
+          options[:links] = (val == 'true') ? true : false
+        end
+      end
+    rescue StandardError => msg
+      puts "Invalid option #{opt_str} (Format --<option>=<value>)"
+    end
+  end
+  if method == 'target' &&
+     ((options[:hashtag].nil? && options[:user].nil?) ||
+     (!options[:hashtag].nil? && !options[:user].nil?))
+    puts "Must provide either a hashtag OR a user"
     return
   end
 
@@ -19,8 +72,10 @@ def main method, tag, num
     # Using Application-only Authentication
     print "Connecting to Twitter... "
     client = Twitter::REST::Client.new do |app|
-      app.consumer_key    = config[:twitter][:consumer_key]
-      app.consumer_secret = config[:twitter][:consumer_secret]
+      app.consumer_key        = config[:twitter][options[:group]][:consumer_key]
+      app.consumer_secret     = config[:twitter][options[:group]][:consumer_secret]
+      app.access_token        = config[:twitter][options[:group]][:access_token]
+      app.access_token_secret = config[:twitter][options[:group]][:access_token_secret]
     end
     puts "success!"
   rescue StandardError => msg
@@ -29,47 +84,52 @@ def main method, tag, num
   end
 
   if method == 'collect'
-    collect client
+    collect client, options
   elsif method == 'target'
-    target client, tag, num
+    target client, options
   end
 end
 
-def target client, tag, num
-  if tag.nil? || tag.empty?
-    puts "Must provide a tag to search for"
-    puts "If you provided a hashtag on the command line,"
-    puts "make sure that it is quote-wrapped"
-    return
-  end
-  tag = tag.strip
-  # make sure it's a hashtag
-  if !(/^#[[:alnum:]]*$/.match(tag))
-    puts "Tag [#{tag}] must be a valid hashtag"
-    return
+def target client, options
+  if options[:send]
+    puts "\nYou're about to send out live tweets."
+    while true
+      print "Are you sure you want to continue? [y/n] "
+      yn = STDIN.gets.chomp.downcase
+      case yn
+      when 'y'
+        break
+      when 'n'
+        return
+      else
+        puts "  Invalid response, enter 'y' or 'n'"
+      end
+    end
   end
 
-  if !(num.nil?) && !(/^[[:digit:]]*$/.match(num))
-    puts "Invalid number of users to generate phishing attacks to (#{num})"
-    return
-  end
-  num ||= 10
-  num = num.to_i
 
-  users = Hash.new
   begin
-    puts "Searching for #{num} recent tweets with #{tag}"
-    tweets = client.search("#{tag} -rt", lang: "en", result_type: "recent").take(num)
-    tweets.each do |tweet|
-      username = tweet.user.screen_name
-      users[username] = Array.new if !(users.include?(username))
-    end
+    if !options[:hashtag].nil?
+      if !(/^#[[:alnum:]]*$/.match(options[:hashtag]))
+        puts "Tag [#{options[:hashtag]}] must be a valid hashtag"
+        return
+      end
 
-    users.each do |username, tweets|
-      getTweetsForUser username, tweets, client
+      users = Array.new
+      puts "Searching for #{options[:count]} recent tweets with #{options[:hashtag]}"
+      tweets = client.search("#{options[:hashtag]} -rt", lang: "en", result_type: "recent").take(options[:count])
+      tweets.each do |tweet|
+        username = tweet.user.screen_name
+        if !(users.include?(username))
+          puts "\nUsername: #{username}\nOriginal Tweet: #{tweet.text}"
+          users.push(username)
+          generateTweet username, options[:send], client
+        end
+      end
+    elsif !options[:user].nil?
+      puts "\nUsername: #{options[:user]}"
+      generateTweet options[:user], options[:send], client
     end
-
-    # puts JSON.pretty_generate(users)
   rescue Twitter::Error::TooManyRequests => msg
     puts "Twitter::Error::TooManyRequests: #{msg}"
     sleep_for = msg.rate_limit.reset_in + 10
@@ -88,6 +148,34 @@ def target client, tag, num
 
 end
 
+def generateTweet username, send, client
+  tweets = Array.new
+  getTweetsForUser username, tweets, client
+
+  if tweets.length > 0
+    len = tweets.length
+    puts "  #{len} tweet#{(len == 1) ? '' : 's'} found)"
+    text = generateTweetText tweets
+    sendTweet username, text, send, client
+  else
+    puts "  No tweets found for #{username}"
+  end
+end
+
+def generateTweetText tweets
+  puts "  Generating a tweet"
+  # This is where we'll use the NN and the recent tweets to generate
+  # a tweet to that user
+  "Some sample return"
+end
+
+def sendTweet username, text, send, client, in_reply_to = nil
+  puts "  #{send ? 'Sending' : 'Would send'} '#{text}' to #{username}"
+  if send
+    client.update("@#{username} #{text}", :in_reply_to_status_id => in_reply_to)
+  end
+end
+
 def collect_with_max_id(collection=[], max_id=nil, &block)
   response = yield(max_id)
   collection += response
@@ -95,46 +183,78 @@ def collect_with_max_id(collection=[], max_id=nil, &block)
 end
 
 def getTweetsForUser username, tweets, client
-  puts "Collecting Tweets for user #{username}"
+  puts "  Collecting recent tweets"
 
-  def client.get_all_tweets(user)
-    collect_with_max_id do |max_id|
-      options = {count: 200, include_rts: true}
-      options[:max_id] = max_id unless max_id.nil?
-      user_timeline(user, options)
+  begin
+    def client.get_all_tweets(user)
+      collect_with_max_id do |max_id|
+        options = {count: 200, include_rts: true}
+        options[:max_id] = max_id unless max_id.nil?
+        user_timeline(user, options)
+      end
     end
-  end
 
-  # cnt = 0
-  client.get_all_tweets(username).each do |tweet|
-    text = tweet.text.gsub("|", " ").gsub("\n", " ").gsub("\r", " ")
-    tweets.push({:id => tweet.id,
-                 :text => text,
-                 :text_abbr => abbreviate(text),
-                 :favorite_count => tweet.favorite_count,
-                 :retweet_count => tweet.retweet_count,
-                 :timestamp => tweet.created_at.to_i})
-    # cnt += 1
-    # break if cnt == 3
+    client.get_all_tweets(username).each do |tweet|
+      text = tweet.text.gsub("|", " ").gsub("\n", " ").gsub("\r", " ")
+      tweets.push({:id => tweet.id,
+                   :text => text,
+                   :text_abbr => abbreviate(text),
+                   :favorite_count => tweet.favorite_count,
+                   :retweet_count => tweet.retweet_count,
+                   :timestamp => tweet.created_at.to_i})
+    end
+  rescue Twitter::Error::NotFound => msg
+    puts "  Username #{username} not found"
+    return
   end
 end
 
-def collect client
-  rslt_file = File.expand_path('tweets.csv')
+def collect client, options
+
+  search = "-rt"
+  search << " filter:links" if options[:links]
+
+  keywords = Array.new
+  rslt_file = ""
+  if !options[:group].nil?
+    puts "Searching for group '#{options[:group]}'"
+    rslt_file = File.expand_path("tweets_#{options[:group]}.csv")
+    begin
+      fname = File.expand_path "#{options[:group]}_keywords.txt"
+      File.open(fname, "r") { |f| f.each_line { |line| keywords.push(line.strip.chomp) } }
+    rescue Errno::ENOENT => msg
+      puts "Failed to load file #{fname}"
+      return
+    end
+
+    keywords_str = keywords.join ' OR '
+    total_len = search.length + keywords_str.length + 1
+    if total_len > 500
+      puts "Search string is too long. Need to reduce by "\
+           "#{total_len-500} characters."
+      return
+    end
+    puts "Keywords are:"
+    puts "  #{keywords.join("\n  ")}"
+    search << " #{keywords_str}"
+  else
+    rslt_file = File.expand_path("tweets.csv")
+  end
+
   cnt, rslts = load_rslts rslt_file
 
   begin
     while true
       begin
         puts "Querying Tweets - Count: #{cnt}"
-        tweets = client.search("filter:links -rt", lang: "en")
+        tweets = client.search(search, lang: "en")
         tweets.each do |tweet|
           to = tweet.in_reply_to_screen_name
           in_reply_to_id = tweet.in_reply_to_status_id
-          next if to.nil? || in_reply_to_id.nil?
+          # next if to.nil? || in_reply_to_id.nil?
 
           from = tweet.user.screen_name
-          next if to == from
+          # next if to == from
           next if to.include?('|') || from.include?('|')
 
           text = tweet.text.gsub("|", " ").gsub("\n", " ").gsub("\r", " ")
@@ -275,4 +395,4 @@ def get_battery_percent
   return perc
 end
 
-main ARGV[0], ARGV[1], ARGV[2]
+main ARGV[0], ARGV[1..-1]
