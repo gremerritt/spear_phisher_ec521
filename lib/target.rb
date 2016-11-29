7c3e7f15fc455332dc33dd04eb74143f1764c02b
gremerritt <gremerritt@gmail.com>
2016-11-28 22:06:09 -0500

    Refactor target script

--- lib/target.rb
+++ lib/target.rb
@@ -3,44 +3,46 @@ module SpearPhisher
   class Targeter
 
     def self.start options
-      if options[:send]
-        puts "\nYou're about to send out live tweets."
-        while true
-          print "Are you sure you want to continue? [y/n] "
-          yn = STDIN.gets.chomp.downcase
-          case yn
-          when 'y'
-            break
-          when 'n'
-            return
-          else
-            puts "  Invalid response, enter 'y' or 'n'"
-          end
-        end
+      raise "Invalid group" if !SpearPhisher.groups.include? options.group
+      @send = options.send == 'true' ? true : false
+
+      if (options.hashtag.nil? && options.user.nil?) ||
+         (!options.hashtag.nil? && !options.user.nil?)
+        puts "Must provide either a hashtag OR a user"
+        return
       end
 
+      if @send
+        puts "\nYou're about to send out live tweets."
+        return if !agree("Are you sure you want to continue? [y/n]")
+      end
+
+      @client = SpearPhisher.setup_client 'default'
 
       begin
-        if !options[:hashtag].nil?
-          if !(/^#[[:alnum:]]*$/.match(options[:hashtag]))
-            puts "Tag [#{options[:hashtag]}] must be a valid hashtag"
-            return
+        if !options.hashtag.nil?
+          if options.hashtag.class == TrueClass
+            raise "Please provide a valid hashtag (wrapped in quotes)"
+          end
+
+          if !(/^#[[:alnum:]]*$/.match(options.hashtag))
+            raise "Tag [#{options.hashtag}] must be a valid hashtag"
           end
 
           users = Array.new
-          puts "Searching for #{options[:count]} recent tweets with #{options[:hashtag]}"
-          tweets = client.search("#{options[:hashtag]} -rt", lang: "en", result_type: "recent").take(options[:count])
+          puts "Searching for #{options.count} recent tweets with #{options.hashtag}"
+          tweets = @client.search("#{options.hashtag} -rt", lang: "en", result_type: "recent").take(options.count)
           tweets.each do |tweet|
             username = tweet.user.screen_name
             if !(users.include?(username))
               puts "\nUsername: #{username}\nOriginal Tweet: #{tweet.text}"
-              users.push(username)
-              generateTweet username, options[:send], client
+              users.push username 
+              generate_tweet username
             end
           end
-        elsif !options[:user].nil?
-          puts "\nUsername: #{options[:user]}"
-          generateTweet options[:user], options[:send], client
+        elsif !options.user.nil?
+          puts "\nUsername: #{options.user}"
+          generate_tweet options.user
         end
       rescue Twitter::Error::TooManyRequests => msg
         puts "Twitter::Error::TooManyRequests: #{msg}"
@@ -60,45 +62,43 @@ module SpearPhisher
 
     end
 
-    def generate_tweet username, send, client
+    def self.generate_tweet username
       tweets = Array.new
-      getTweetsForUser username, tweets, client
+      get_tweets_for_user username, tweets
 
       if tweets.length > 0
         len = tweets.length
         puts "  #{len} tweet#{(len == 1) ? '' : 's'} found"
-        text = generateTweetText tweets
-        sendTweet username, text, send, client
+        text = generate_tweet_text tweets
+        send_tweet username, text
       else
         puts "  No tweets found for #{username}"
       end
     end
 
-    def generate_tweet_text tweets
+    def self.generate_tweet_text tweets
       puts "  Generating a tweet"
       # This is where we'll use the NN and the recent tweets to generate
       # a tweet to that user
       "Some sample return"
     end
 
-    def send_tweet username, text, send, client, in_reply_to = nil
-      puts "  #{send ? 'Sending' : 'Would send'} '#{text}' to #{username}"
-      if send
-        client.update("@#{username} #{text}", :in_reply_to_status_id => in_reply_to)
-      end
+    def self.send_tweet username, text, in_reply_to = nil
+      puts "  #{@send ? 'Sending' : 'Would send'} '#{text}' to #{username}"
+      @client.update("@#{username} #{text}", :in_reply_to_status_id => in_reply_to) if @send
     end
 
-    def collect_with_max_id(collection=[], max_id=nil, &block)
+    def self.collect_with_max_id(collection=[], max_id=nil, &block)
       response = yield(max_id)
       collection += response
       response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
     end
 
-    def self.get_tweets_for_user username, tweets, client
+    def self.get_tweets_for_user username, tweets
       puts "  Collecting recent tweets"
 
       begin
-        def client.get_all_tweets(user)
+        def @client.get_all_tweets(user)
           collect_with_max_id do |max_id|
             options = {count: 200, include_rts: true}
             options[:max_id] = max_id unless max_id.nil?
@@ -106,11 +106,11 @@ module SpearPhisher
           end
         end
 
-        client.get_all_tweets(username).each do |tweet|
+        @client.get_all_tweets(username).each do |tweet|
           text = tweet.text.gsub("|", " ").gsub("\n", " ").gsub("\r", " ")
           tweets.push({:id => tweet.id,
                        :text => text,
-                       :text_abbr => abbreviate(text),
+                       :text_abbr => SpearPhisher.abbreviate(text),
                        :favorite_count => tweet.favorite_count,
                        :retweet_count => tweet.retweet_count,
                        :timestamp => tweet.created_at.to_i})
